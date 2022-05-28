ab7f14afde31e7036cf9c8249194cd7448f8963f
Omer-Cicek <omercicek160408@gmail.com>
2022-05-28 10:09:01 +0300

    accounts list page added

--- src/pages/CustomerAccList.js
+++ src/pages/CustomerAccList.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import CustomerAccountListService from '../services/CustomerAccountListService';
 
 const CustomerAccList = () => {
+  const [userAccounts, setUserAccounts] = useState([]);
+
   const {
     state: { id },
   } = useLocation();
-  console.log(id);
 
   const formData = new FormData();
 
@@ -17,6 +19,7 @@ const CustomerAccList = () => {
 
   const handleAccountList = () => {
     CustomerAccountListService.getAll(id).then((res) => {
+      setUserAccounts(res.data.Result);
       console.log(res);
     });
   };
@@ -25,7 +28,39 @@ const CustomerAccList = () => {
     handleAccountList();
   }, []);
 
-  return <div>CustomerAccList</div>;
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Full Name</th>
+            <th>Account No</th>
+            <th>Customer Id</th>
+            <th>Base Currency Id</th>
+          </tr>
+        </thead>
+        {userAccounts?.map((account, index) => {
+          return (
+            <tbody key={account.AccountId}>
+              <tr>
+                <td>{index + 1}</td>
+                <td>
+                  {account.FirstName} {account.LastName}
+                </td>
+                <td>{account.AccountNo}</td>
+                <td>{account.CustomerId}</td>
+                <td>{account.BaseCurrencyId}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </Table>
+      {userAccounts.length === 0 && (
+        <h2 className="lead text-center">User has no active account</h2>
+      )}
+    </>
+  );
 };
 
 export default CustomerAccList;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import { BsFillArrowRightSquareFill } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
   const [customerInfo, setCustomerInfo] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   const { loggedIn } = useSelector((state) => state.LoginReducer);
 
   const navigate = useNavigate();
@@ -18,7 +19,7 @@ const Main = () => {
   formData.append('Version', '1');
   formData.append('Parameters', '{}');
 
-  const handleCustomers = (e) => {
+  const handleCustomers = () => {
     axios(
       'https://dev-smoothie-api.fintechyazilim.com/api/v1/FinTech/Execute',
       {
@@ -30,11 +31,16 @@ const Main = () => {
       .then((data) => {
         setCustomerInfo(data.data.Result);
         console.log('DATAUSERS', data.data);
+        setIsLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
+    setIsLoading(true);
     handleCustomers();
   }, []);
 
@@ -43,39 +49,48 @@ const Main = () => {
   };
 
   return (
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Birth Date</th>
-          <th>Email</th>
-          <th>User Account's List</th>
-        </tr>
-      </thead>
-      <tbody>
-        {customerInfo?.map((customer, index) => {
-          return (
-            <tr key={customer.CustomerId}>
-              <td>{index + 1}</td>
-              <td>{customer.FirstName}</td>
-              <td>{customer.LastName}</td>
-              <td>{customer.BirthDate}</td>
-              <td>{customer.Email}</td>
-              <td>
-                <Button
-                  variant="info"
-                  onClick={() => handleUserAccounts(customer.CustomerId)}
-                >
-                  Accounts <BsFillArrowRightSquareFill />
-                </Button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+    <>
+      {isLoading && (
+        <Spinner
+          animation="border"
+          className="text-center"
+          variant="secondary"
+        />
+      )}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Birth Date</th>
+            <th>Email</th>
+            <th>User Account's List</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customerInfo?.map((customer, index) => {
+            return (
+              <tr key={customer.CustomerId}>
+                <td>{index + 1}</td>
+                <td>{customer.FirstName}</td>
+                <td>{customer.LastName}</td>
+                <td>{customer.BirthDate}</td>
+                <td>{customer.Email}</td>
+                <td>
+                  <Button
+                    variant="info"
+                    onClick={() => handleUserAccounts(customer.CustomerId)}
+                  >
+                    Accounts <BsFillArrowRightSquareFill />
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
